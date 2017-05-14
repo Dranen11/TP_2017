bb2fbc6cfd8af3258490e28fc2c4cbb42f92342d
Mathieu de Bony <mathieu.de-bony-de-lavergne@u-psud.fr>
2017-05-14 20:31:22 +0200

    Modification de la sauvegarde des graphiques

--- Python/Graphique.py
+++ Python/Graphique.py
@@ -1,4 +1,4 @@
-def Graphique_Simple(x, y,x_label = "",y_label = "", graph_name = "", linestyle = 'b-'):
+def Graphique_Simple(x, y,x_label = "",y_label = "", graph_name = "", linestyle = 'b-', save_name = "",):
     import numpy as np
     import matplotlib.pyplot as plt
 
@@ -8,11 +8,15 @@ def Graphique_Simple(x, y,x_label = "",y_label = "", graph_name = "", linestyle 
     plt.ylabel(y_label)
     plt.title(graph_name)
 
-    plt.savefig(graph_name + '.eps', format = 'eps')
-    plt.savefig(graph_name + '.svg', format = 'svg')
+    if save_name == "":
+        plt.savefig(graph_name + '.eps', format = 'eps')
+        plt.savefig(graph_name + '.svg', format = 'svg')
+    else :
+        plt.savefig(save_name + '.eps', format = 'eps')
+        plt.savefig(save_name + '.svg', format = 'svg')
     plt.show(block = False)
 
-def Graphique_Double_Echelle(x, y1, y2,x_label = "",y1_label = "", y2_label = "", graph_name = "", linestyle1 = '-', linestyle2 = '-', color1 = 'b', color2 = 'g'):
+def Graphique_Double_Echelle(x, y1, y2,x_label = "",y1_label = "", y2_label = "", graph_name = "", save_name = "", linestyle1 = '-', linestyle2 = '-', color1 = 'b', color2 = 'g'):
     import numpy as np
     import matplotlib.pyplot as plt
 
@@ -25,6 +29,10 @@ def Graphique_Double_Echelle(x, y1, y2,x_label = "",y1_label = "", y2_label = ""
     ax2.set_ylabel(y2_label, color = color2)
     plt.title(graph_name)
 
-    plt.savefig(graph_name + '.eps', format = 'eps')
-    plt.savefig(graph_name + '.svg', format = 'svg')
+    if save_name == "":
+        plt.savefig(graph_name + '.eps', format = 'eps')
+        plt.savefig(graph_name + '.svg', format = 'svg')
+    else :
+        plt.savefig(save_name + '.eps', format = 'eps')
+        plt.savefig(save_name + '.svg', format = 'svg')
     plt.show(block = False)
