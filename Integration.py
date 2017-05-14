ed05ccc5baa4fd106bf41fd2922727331e43342c
Mathieu de Bony <mathieu.de-bony-de-lavergne@u-psud.fr>
2017-05-14 16:16:00 +0200

    Correction de l'erreur sur la boucle for dans Integration

--- Integration.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def Integrale_Trapeze(temps,valeur):
-    integral = 0.;
-    for i in np.arrange(temps.ndarray.size-2):
-        integral += 0.5*(valeur[i]+valeur[i+1])*(temps[i+1]-temps[i])
-    return integral
--- Python/Integration.py
+++ Python/Integration.py
@@ -1,6 +1,6 @@
 def Integrale_Trapeze(temps,valeur):
     import numpy as np
     integral = 0.;
-    for i in np.arange(temps.size-2):
+    for i in range(0,temps.size-1):
         integral += 0.5*(valeur[i]+valeur[i+1])*(temps[i+1]-temps[i])
     return integral
