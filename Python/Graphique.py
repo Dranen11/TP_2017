f7bdbaeaa07ab2c7d0004b0b50461a8dd6c88e3c
Mathieu de Bony <mathieu.de-bony-de-lavergne@u-psud.fr>
2017-05-14 20:19:30 +0200

    Ajout d'une fonction pour faire un graph avec deux axes y

--- Python/Graphique.py
+++ Python/Graphique.py
@@ -1,11 +1,28 @@
-def Graphique_Simple(x, y,x_label = "",y_label = "", graph_name = ""):
+def Graphique_Simple(x, y,x_label = "",y_label = "", graph_name = "", linestyle = 'b-'):
     import numpy as np
     import matplotlib.pyplot as plt
 
     plt.figure(figsize=(16,6))
-    plt.plot(x, y)
+    plt.plot(x, y, linestyle)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.title(graph_name)
+
+    plt.savefig(graph_name + '.eps', format = 'eps')
+    plt.savefig(graph_name + '.svg', format = 'svg')
+    plt.show(block = False)
+
+def Graphique_Double_Echelle(x, y1, y2,x_label = "",y1_label = "", y2_label = "", graph_name = "", linestyle1 = '-', linestyle2 = '-', color1 = 'b', color2 = 'g'):
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.plot(x, y1, color1+linestyle1)
+    ax2.plot(x, y2, color1+linestyle2)
+    ax1.set_xlabel(x_label)
+    ax1.set_ylabel(y1_label, color = color1)
+    ax2.set_ylabel(y2_label, color = color2)
     plt.title(graph_name)
 
     plt.savefig(graph_name + '.eps', format = 'eps')
