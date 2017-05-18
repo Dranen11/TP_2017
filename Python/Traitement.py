706b3b5de300c689632ccaa5353e4ca6240c1993
Mathieu de Bony <mathieu.de-bony-de-lavergne@u-psud.fr>
2017-05-18 15:41:04 +0200

    Correction d'erreur

--- Python/Traitement.py
+++ Python/Traitement.py
@@ -7,9 +7,9 @@ def Traitement(nom_exp, valeur_resistance, amplification = 1., diviseur = 1., of
     from Integration import Integrale_Trapeze
     from Filtre import Passe_bas
 
-    data = np.loadtxt(nom_exp+'.dat')
-    Temps = data[:,1]/1000000.
-    Tension = (data[:,2]-offset)*diviseur/amplification
+    data = np.loadtxt(nom_exp+'.dat', delimiter = ';')
+    Temps = np.asarray(data[:,0], dtype=float)/1000000.
+    Tension = (np.asarray(data[:,1], dtype=float)-offset)*diviseur/amplification
     Intensite = Tension/(valeur_resistance);
 
     if freq_coupure != 0:
@@ -19,9 +19,10 @@ def Traitement(nom_exp, valeur_resistance, amplification = 1., diviseur = 1., of
     Puissance = Tension*Intensite;
 
     Graphique_Double_Echelle(Temps, Tension, Intensite, x_label = 'Temps [s]', y1_label = 'Tension [V]', y2_label = 'Intensite [A]', save_name = 'Graphique_'+nom_exp+'_TI', graph_name = 'Evolution de la tension et du courant au cours du temps')
-    Graphique_Simple(Temps, Puissance, y_label = 'Puissance [W]', x_label = 'Temps [s]', graph_name = 'Evolution de la puissance électrique au cours du temps', save_name = 'Graphique_'+nom_exp+'_P')
+    Graphique_Simple(Temps, Tension, x_label = 'Temps [s]', y_label = 'Tension [V]', save_name = 'Graphique_'+nom_exp+'_T', graph_name = 'Evolution de la tension au cours du temps')
+    Graphique_Simple(Temps, Puissance, y_label = 'Puissance [W]', x_label = 'Temps [s]', graph_name = 'Evolution de la puissance electrique au cours du temps', save_name = 'Graphique_'+nom_exp+'_P')
     Energie = Integrale_Trapeze(Temps, Puissance)
-    print("Energie : " + Energie + " J")
+    print("Energie : " + str(Energie) + " J")
 
     return Energie
 
@@ -30,14 +31,14 @@ def Traitement(nom_exp, valeur_resistance, amplification = 1., diviseur = 1., of
 if __name__ == '__main__':
     print 'sys.argv: ', sys.argv
     if len(sys.argv) == 3:
-        Traitement(sys.argv[1], sys.argv[2])
+        Traitement(sys.argv[1], float(sys.argv[2]))
     elif len(sys.argv) == 4:
-        Traitement(sys.argv[1], sys.argv[2], sys.argv[3])
+        Traitement(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]))
     elif len(sys.argv) == 5:
-        Traitement(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        Traitement(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]))
     elif len(sys.argv) == 6:
-        Traitement(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+        Traitement(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]))
     elif len(sys.argv) == 7:
-        Traitement(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+        Traitement(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
     else:
         message('No argument')
