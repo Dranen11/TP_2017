1c89323ace436deddb2e0b2d623838e8d52baa6a
Mathieu de Bony <mathieu.de-bony-de-lavergne@u-psud.fr>
2017-05-21 17:20:15 +0200

    Correction d'erreur et finalisationde Traitement Difference

---
+++ Python/Traitement_Difference.py
@@ -0,0 +1,70 @@
+import sys
+
+def Traitement_Difference(nom_exp1, nom_exp2, nom_resultat, valeur_resistance, t1, t2, duree,amplification1 = 1., amplification2 = 1., diviseur = 1., offset = 0., freq_coupure = 0.):
+    import numpy as np
+    from Graphique import Graphique_Simple
+    from Graphique import Graphique_Double_Echelle
+    from Integration import Integrale_Trapeze
+    from Filtre import Passe_bas
+
+    data1 = np.loadtxt(nom_exp1+'.dat', delimiter = ';')
+    data2 = np.loadtxt(nom_exp2+'.dat', delimiter = ';')
+
+    Temps1 = np.asarray(data1[:,0], dtype=float)/1000000.
+    Temps1 = Temps1 - Temps1[0]
+    Tension1 = (np.asarray(data1[:,1], dtype=float)-offset)*diviseur/amplification1
+
+    Temps2 = np.asarray(data2[:,0], dtype=float)/1000000.
+    Temps2 = Temps2 - Temps2[0]
+    Tension2 = (np.asarray(data2[:,1], dtype=float)-offset)*diviseur/amplification1
+
+    if freq_coupure != 0:
+            Tension1 = Passe_bas(Temps1, Tension1, freq_coupure)
+            Tension1 = Passe_bas(Temps1, Tension1, freq_coupure)
+
+    i1 = 0
+    for i in range(0, Temps1.size):
+        if abs(Temps1[i1]-t1) > abs(Temps1[i]-t1) :
+            i1 = i
+    di = 0
+    for i in range(i1, Temps1.size):
+        if abs(Temps1[di+i1]-(t1+duree)) > abs(Temps1[i]-(t1+duree)) :
+            di = i - i1
+
+    i2 = 0
+    for i in range(0, Temps2.size):
+        if abs(Temps2[i2]-t2) > abs(Temps2[i]-t2) :
+            i2 = i
+
+    Temps = np.zeros(di)
+    T1 = np.zeros(di)
+    T2 = np.zeros(di)
+
+    for i in range(i1, i1+di):
+        Temps[i-i1] = Temps1[i]-Temps1[i1]
+        T1[i-i1] = Tension1[i1]
+
+    for i in range(i2, i2+di):
+        T2[i-i2] = Tension2[i2]
+
+    Tension = T1-T2
+
+    Graphique_Simple(Temps, Tension, x_label = 'Temps [s]', y_label = 'Tension [V]', save_name = 'Graphique_'+nom_resultat+'_T', graph_name = 'Difference de la tension au cours du temps')
+
+
+
+
+if __name__ == '__main__':
+    print 'sys.argv: ', sys.argv
+    if len(sys.argv) == 8:
+        Traitement_Difference(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]), float(sys.argv[7]))
+    elif len(sys.argv) == 10:
+        Traitement_Difference(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]), float(sys.argv[7]), float(sys.argv[8]), float(sys.argv[9]))
+    elif len(sys.argv) == 11:
+        Traitement_Difference(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]), float(sys.argv[7]), float(sys.argv[8]), float(sys.argv[9]), float(sys.argv[10]))
+    elif len(sys.argv) == 12:
+        Traitement_Difference(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]), float(sys.argv[7]), float(sys.argv[8]), float(sys.argv[9]), float(sys.argv[10]), float(sys.argv[11]))
+    elif len(sys.argv) == 13:
+        Traitement_Difference(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]), float(sys.argv[7]), float(sys.argv[8]), float(sys.argv[9]), float(sys.argv[10]), float(sys.argv[11]), float(sys.argv[12]))
+    else:
+        message('No argument')
