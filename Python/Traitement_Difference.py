d0568de126548e0bc63f5e530c170f09f9fe3826
Mathieu de Bony <mathieu.de-bony-de-lavergne@u-psud.fr>
2017-05-21 17:44:17 +0200

    Corection et Ajout FFT

--- Python/Traitement_Difference.py
+++ Python/Traitement_Difference.py
@@ -6,6 +6,7 @@ def Traitement_Difference(nom_exp1, nom_exp2, nom_resultat, valeur_resistance, t
     from Graphique import Graphique_Double_Echelle
     from Integration import Integrale_Trapeze
     from Filtre import Passe_bas
+    from scipy import fftpack
 
     data1 = np.loadtxt(nom_exp1+'.dat', delimiter = ';')
     data2 = np.loadtxt(nom_exp2+'.dat', delimiter = ';')
@@ -17,10 +18,6 @@ def Traitement_Difference(nom_exp1, nom_exp2, nom_resultat, valeur_resistance, t
     Temps2 = np.asarray(data2[:,0], dtype=float)/1000000.
     Temps2 = Temps2 - Temps2[0]
     Tension2 = (np.asarray(data2[:,1], dtype=float)-offset)*diviseur/amplification1
-
-    if freq_coupure != 0:
-            Tension1 = Passe_bas(Temps1, Tension1, freq_coupure)
-            Tension1 = Passe_bas(Temps1, Tension1, freq_coupure)
 
     i1 = 0
     for i in range(0, Temps1.size):
@@ -42,15 +39,20 @@ def Traitement_Difference(nom_exp1, nom_exp2, nom_resultat, valeur_resistance, t
 
     for i in range(i1, i1+di):
         Temps[i-i1] = Temps1[i]-Temps1[i1]
-        T1[i-i1] = Tension1[i1]
+        T1[i-i1] = Tension1[i]
 
     for i in range(i2, i2+di):
-        T2[i-i2] = Tension2[i2]
+        T2[i-i2] = Tension2[i]
 
     Tension = T1-T2
+    freq_echantillonage = Temps[Temps.size-1]/Temps.size
+    freqs = fftpack.fftfreq(Temps.size, d=freq_echantillonage)
+    TFT_M = abs(fftpack.fft(Tension-np.mean(Tension)))
+    TFT = abs(fftpack.fft(Tension))
 
     Graphique_Simple(Temps, Tension, x_label = 'Temps [s]', y_label = 'Tension [V]', save_name = 'Graphique_'+nom_resultat+'_T', graph_name = 'Difference de la tension au cours du temps')
-
+    Graphique_Simple(freqs, TFT, x_label = 'Temps [s]', y_label = 'Tension [V]', save_name = 'Graphique_'+nom_resultat+'_TFT', graph_name = 'TF Difference de la tension au cours du temps')
+    Graphique_Simple(freqs, TFT_M, x_label = 'Temps [s]', y_label = 'Tension [V]', save_name = 'Graphique_'+nom_resultat+'_TTF-M', graph_name = 'TF sans la composante continu Difference de la tension au cours du temps')
 
 
 
